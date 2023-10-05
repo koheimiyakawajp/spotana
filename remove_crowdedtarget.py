aa72d853391c533a75240b190db7298b1a0a39b2
koheimiyakawajp <miyakawa.geo@gmail.com>
2023-10-05 17:21:10 +0900

    minor modfy

--- remove_crowdedtarget.py
+++ remove_crowdedtarget.py
@@ -23,11 +23,15 @@ def main(args):
     flg_ar      = [True]
     for dline in dlist[1:]:
         epicid  = dline[0]
-        contam_val  = dcontam[(dcontam[:,0]==epicid)][0]
-        print(contam_val)
-        cwd     = float(contam_val[1])
-        if cwd > 0.8:
-            flg_ar.append(True)
+        contam_val  = dcontam[(dcontam[:,0]==epicid)]
+        if np.ndim(contam_val) ==2:
+            contam_val   = contam_val[0]
+            print(contam_val)
+            cwd     = float(contam_val[1])
+            if cwd > 0.8:
+                flg_ar.append(True)
+            else:
+                flg_ar.append(False)
         else:
             flg_ar.append(False)
     np.savetxt(ftarlist.split(".")[0] + "_rmcnt.csv", dlist[flg_ar], delimiter=',', fmt="%s")
